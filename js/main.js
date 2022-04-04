1ed56dd63c72c167324ba3ce0b42191bae5c1946
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-04-04 09:16:34 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ import {
   clearStatsLine,
   setStatsLine
 } from "./searchResults.js";
+
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js";
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
