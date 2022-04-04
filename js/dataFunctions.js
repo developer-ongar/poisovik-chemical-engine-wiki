10c5377d528bc7c9675fe1b068dc792b45dae397
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-04-04 09:14:47 +0600

    Update dataFunctions.js

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -21,3 +21,12 @@ const getWikiSearchString = (searchTerm) => {
   const searchString = encodeURI(rawSearchString);
   return searchString;
 };
+
+const getMaxChars = () => {
+  const width = window.innerWidth || document.body.clientWidth;
+  let maxChars;
+  if (width < 414) maxChars = 65;
+  if (width >= 414 && width < 1400) maxChars = 100;
+  if (width >= 1400) maxChars = 130;
+  return maxChars;
+};
